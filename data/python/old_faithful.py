cb3d832c2611db37cc73ae8e9f92049ed5ce00a5
Eedrah <eedrah@users.noreply.github.com>
2019-04-22 01:37:55 +1200

    restructure data locations

--- data/python/old_faithful.py
+++ data/python/old_faithful.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('../../../data/old-faithful/OldFaithful.csv', delimiter=',', names=('duration', 'delay'), usecols=(1,2), skip_header=1)
+data = np.genfromtxt('../raw/old_faithful/OldFaithful.csv', delimiter=',', names=('duration', 'delay'), usecols=(1,2), skip_header=1)
 
 def main():
     plt.scatter(data['duration'], data['delay'])
