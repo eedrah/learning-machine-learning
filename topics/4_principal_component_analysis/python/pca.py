7ff321ca0554fbbb671dcd7af4b46fa881855d75
Eedrah <eedrah@users.noreply.github.com>
2019-05-03 01:23:02 +1200

    run python fixer

--- topics/4_principal_component_analysis/python/pca.py
+++ topics/4_principal_component_analysis/python/pca.py
@@ -1,8 +1,10 @@
+""" Principal component analysis """
 import sys
-sys.path.append('../../..')
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+sys.path.append("../../..")
 from data.python.old_faithful import data
 
 normalized_data = (data - data.mean()) / data.std()
@@ -15,24 +17,29 @@ eigenvectors = eigenvectors[descending_indices]
 
 principal_components = normalized_data @ eigenvectors
 
-projections = [np.outer(principal_component, eigenvector)
-        for ((_, principal_component), eigenvector)
-        in zip(principal_components.iteritems(), eigenvectors.T)]
+projections = [
+    np.outer(principal_component, eigenvector)
+    for ((_, principal_component), eigenvector) in zip(
+        principal_components.iteritems(), eigenvectors.T
+    )
+]
+
 
 def main():
-    plt.style.use('seaborn')
-    plt.title('Principal component analysis of Old Faithful')
+    """Display the PCA on a graph"""
+    plt.style.use("seaborn")
+    plt.title("Principal component analysis of Old Faithful")
 
-    plt.xlabel([*normalized_data][0] + ' (normalized)')
-    plt.ylabel([*normalized_data][1] + ' (normalized)')
-    plt.axis('equal')
+    plt.xlabel([*normalized_data][0] + " (normalized)")
+    plt.ylabel([*normalized_data][1] + " (normalized)")
+    plt.axis("equal")
 
-    plt.scatter(normalized_data['duration'], normalized_data['delay'], alpha=0.4)
+    plt.scatter(normalized_data["duration"], normalized_data["delay"], alpha=0.4)
     for projection in projections:
         plt.scatter(projection[:, 0], projection[:, 1], alpha=0.4)
 
     plt.show()
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
