50ef9b5ad19d14fe3e2475d8a4eb23f3f3bbdfc5
Eedrah <9520856+eedrah@users.noreply.github.com>
2020-01-20 22:00:13 +1300

    WIP

--- topics/3_gaussian_mixture_models_and_expectation_maximization/python/expectation_maximization.py
+++ topics/3_gaussian_mixture_models_and_expectation_maximization/python/expectation_maximization.py
@@ -1,8 +1,22 @@
+"""
+(data, number of clusters, init=(optional soft cluster assignments), return-goodness-of-fit-for-each-iteration?) -> (k gaussian distributions (mean dx1, cov dxd, mixture coefficient), n x k matrix of soft cluster assignments, goodness of fit)
+    initialize clusters by randomly assigning soft cluster assignments
+    compute mean, cov for each cluster, weighted on soft cluster assignment
+    sum soft cluster assignments, divide by N for mixture coefficients
+
+    for every datapoint and cluster, calculate pdf at the data point, weight, divide through by sum, to get soft cluster assignments
+
+    calculate sum squared likelihood for each cluster, each datapoint proportional to the pdf
+        sum across clusters
+        = goodness of fit -> maximize this
+
+    end when mean and std are stable
+"""
 import numpy as np
 from scipy.stats import multivariate_normal
 
 
-def expectation_maximization(data, number_of_clusters):
+class ExpectationMaximization:
     def initialize_clusters(number_of_observations, number_of_clusters):
         random_numbers = np.random.rand(number_of_observations, number_of_clusters)
         return random_numbers / random_numbers.sum(0)
@@ -36,41 +50,30 @@ def expectation_maximization(data, number_of_clusters):
         soft_cluster_assignments = pdfs / pdfs.sum(0)
         return soft_cluster_assignments, log_likelihood
 
-    max_iterations = 1e7
-    tolerance = 1e-2
+    def __init__(data, number_of_clusters):
+        max_iterations = 1e7
+        tolerance = 1e-2
 
-    soft_cluster_assignments = initialize_clusters(data.shape[0], number_of_clusters)
-    did_converge = False
-    for _ in range(max_iterations):
-        means, covariances, mixture_coefficients = compute_mean_cov_mixture_coefficients(
-            data, soft_cluster_assignments
+        soft_cluster_assignments = initialize_clusters(
+            data.shape[0], number_of_clusters
         )
+        did_converge = False
+        for _ in range(max_iterations):
+            means, covariances, mixture_coefficients = compute_mean_cov_mixture_coefficients(
+                data, soft_cluster_assignments
+            )
 
-        previous_log_likelihood = log_likelihood
-        soft_cluster_assignments, log_likelihood = calculate_soft_cluster_assignments(
-            data, means, covariances, mixture_coefficients
+            previous_log_likelihood = log_likelihood
+            soft_cluster_assignments, log_likelihood = calculate_soft_cluster_assignments(
+                data, means, covariances, mixture_coefficients
+            )
+            if log_likelihood - previous_log_likelihood < tolerance:
+                did_converge = True
+                break
+
+        return (
+            did_converge,
+            (means, covariances, mixture_coefficients),
+            soft_cluster_assignments,
+            log_likelihood,
         )
-        if log_likelihood - previous_log_likelihood < tolerance:
-            did_converge = True
-            break
-    return (
-        did_converge,
-        (means, covariances, mixture_coefficients),
-        soft_cluster_assignments,
-        log_likelihood,
-    )
-
-    """
-(data, number of clusters, init=(optional soft cluster assignments), return-goodness-of-fit-for-each-iteration?) -> (k gaussian distributions (mean dx1, cov dxd, mixture coefficient), n x k matrix of soft cluster assignments, goodness of fit)
-    initialize clusters by randomly assigning soft cluster assignments
-    compute mean, cov for each cluster, weighted on soft cluster assignment
-    sum soft cluster assignments, divide by N for mixture coefficients
-
-    for every datapoint and cluster, calculate pdf at the data point, weight, divide through by sum, to get soft cluster assignments
-
-    calculate sum squared likelihood for each cluster, each datapoint proportional to the pdf
-        sum across clusters
-        = goodness of fit -> maximize this
-
-    end when mean and std are stable
-"""
---
+++ topics/3_gaussian_mixture_models_and_expectation_maximization/python/test_expectation_maximization.py
@@ -0,0 +1,12 @@
+import unittest
+import ExpectationMaximization from
+
+
+class TestExpectationMaximization(unittest.TestCase):
+    def test_initialization(self):
+
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
